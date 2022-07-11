babfedc030683b1a2ca4c3551c030b3a8a037a6e
Jesusgomrec <90487411+Chuchogom@users.noreply.github.com>
2022-07-11 13:29:31 -0500

    Add navbar

---
+++ src/navbar/navBar.js
@@ -0,0 +1,26 @@
+import logo from '../logo.svg';
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav className='navbar'>
+        <h1 className='navbar-title'> <img src={logo} className="App-logo" alt="logo" /> Cinema App </h1>
+            <ul className='navbar-list'>
+                <li className='navbar-item'>
+                    <a href='/home'>Home</a>
+                </li>
+                <li className='navbar-item'>
+                    <a href='/movies'>Movies</a>
+                </li>
+                <li className='navbar-item'>
+                    <a href='/profile'>Profile</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
+
